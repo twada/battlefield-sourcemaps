2babc6b77184fc2694a890cced6e8de8aba215bf
Takuto Wada <takuto.wada@gmail.com>
2016-02-08 00:50:19 +0900

    chore(tests): adjust with latest mocha output

--- test/verification/es6_output_test.js
+++ test/verification/es6_output_test.js
@@ -6,9 +6,9 @@ var assertionLineNumberTest = require('./line-number');
 describe('ES6 power-assert output verification', function () {
 
     assertionLineNumberTest('ES6', actualOutputPath, [
-            /^\s*AssertionError:\s*\#\s*(?:test\/(?:node|web)\/)?arrowfunction_and_spread_test.es6\:7$/,
-            /^\s*AssertionError:\s*\#\s*(?:test\/(?:node|web)\/)?destructuring_and_templateliteral_test.es6\:7$/,
-            /^\s*AssertionError:\s*\#\s*(?:test\/(?:node|web)\/)?enhanced_object_literal_test.es6\:7$/
+            /^\s*\#\s*(?:test\/(?:node|web)\/)?arrowfunction_and_spread_test.es6\:7$/,
+            /^\s*\#\s*(?:test\/(?:node|web)\/)?destructuring_and_templateliteral_test.es6\:7$/,
+            /^\s*\#\s*(?:test\/(?:node|web)\/)?enhanced_object_literal_test.es6\:7$/
     ]);
 
     diagramRenderingTest('ES6', actualOutputPath, [
